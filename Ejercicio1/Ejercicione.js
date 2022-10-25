85d8d15b2d748046ff21e14dbe9d108287d3c5bf
Cristhian <cristhiangomezdev@gmail.com>
2022-10-25 16:40:57 +0200

    Loops

---
+++ Ejercicio1/Ejercicione.js
@@ -0,0 +1,24 @@
+
+// ejercicio 1
+// for (let i = 0; i <=50; i++) 
+// {if(i%15 ===0 || i%10 ===0){
+//     console.log('Donkey')
+
+
+// }else if  (i%2 != 0 && (i-1)%10===0) {
+//     console.log('Monkey')
+// } else if(i%5===0){
+//     continue
+// }
+//      console.log(i)
+
+// }
+
+
+
+   
+
+
+
+    
+
---
+++ Ejercicio2/ejerciciotwo.js
@@ -0,0 +1,57 @@
+//ejercicio 2
+  let num1 = prompt('ingrese el número deseado')
+  for (let i = 0; i <= num1; i++) {
+      console.log(i)
+    }
+// con while
+// let num8 = prompt('ingrese el número deseado')
+// let i =0
+// while (i<=num8) {
+//     console.log(i)
+//     i++
+    
+// }
+
+//  
+//ejercicio 3
+//  let num1 = prompt('ingrese el número deseado')
+//  for (let i = 0; i <= num1; i=i+2) {
+//      console.log(i)
+
+//Con While
+// let num8 = prompt('ingrese el número deseado')
+// let i =0
+// while (i<=num8) {
+//     console.log(i)
+//     i=i+2
+    
+// }
+    
+//  }
+
+ //ejercicio 4
+
+//  let num1 = prompt('ingrese el número deseado')
+//  for (let i = num1; i >= 0; i--) {
+//      console.log(i)
+    
+//  }
+
+ //ejercicio 5
+
+ 
+//  const num3=9
+
+//  for(let i=0; i<=10; i++){
+//     console.log(i*num3 )
+//  }
+
+//ejercicio 7
+// let M = prompt('Ingrese el primer número')
+// let N = prompt('Ingrese el segundo número')
+// let suma =0
+
+//     for (let j = M; j <= N; j++) {
+//         console.log(j)  
+//     }
+    
